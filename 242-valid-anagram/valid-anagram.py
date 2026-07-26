2b03d2bbe3053ab19800fc38da997a65db85ac64
Aahana Tyagi <154831617+normajean8@users.noreply.github.com>
2026-07-27 02:10:44 +0530

    Time: 12 ms (55.99%) | Memory: 19.3 MB (75.33%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen={}
+        frequency={}
+
+        for ch in s:
+            if ch in seen:
+
+                seen[ch]+=1
+            else:
+
+                seen[ch]=1
+
+        for cj in t:
+            if cj in frequency:
+
+                frequency[cj]+=1
+            else:
+
+                frequency[cj]=1
+
+        if seen==frequency:
+            return True
+
+        else:
+            return False
+        
