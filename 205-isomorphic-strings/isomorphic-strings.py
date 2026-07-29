b0cf898f46f55d21787cf1fa46d009e197738dac
Aahana Tyagi <154831617+normajean8@users.noreply.github.com>
2026-07-29 22:08:01 +0530

    Time: 3 ms (91.01%) | Memory: 19.2 MB (46.04%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        seen={}
+        freq={}
+
+        for i in range(len(s)):
+            ch=s[i]
+            cj=t[i]
+
+            if ch in seen:
+                if seen[ch]!=cj:
+                    return False
+
+            else:
+                 seen[ch]=cj
+
+            if cj in freq:
+                if freq[cj]!=ch:
+                    return False
+
+            else:
+                freq[cj]=ch
+
+        return True
+
+
+        
